b0647eafa26c3c3d106213c3921eb46fd188087f
Ross <ejones0401@gmail.com>
2020-09-15 20:24:39 -0400

    pushy push

--- mod4solution/js/script.js
+++ mod4solution/js/script.js
@@ -1,38 +1,65 @@
-// Closures
-function makeMultiplier (multiplier) {
-	function b() {
-		console.log("Multiplier is: " + multiplier);
-	}
-	b();
-	return (
-		function (x) {
-			return multiplier * x;
-		} //This is NOT an execution of a function (just saving it to a variable) so it doesnt get an outer reference, this object, etc.
-		) //^ UNTIL it is called later!!
+// *******************************
+// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+// Module 4 Assignment Instructions.
+//
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// or "j", otherwise, the program should say "Good Bye". So, the final output
+// on the console should look like this:
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+
+WARNING!!! WARNING!!!
+The code does NOT currently work! It is YOUR job to make it work
+as described in the requirements and the steps in order to complete this
+assignment.
+WARNING!!! WARNING!!!
+
+*/
+
+// STEP 1:
+// Wrap the entire contents of script.js inside of an IIFE
+// See Lecture 52, part 2
+// (Note, Step 2 will be done in the SpeakHello.js file.)
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+// STEP 10:
+// Loop over the names array and say either 'Hello' or "Good Bye"
+// using the 'speak' method or either helloSpeaker's or byeSpeaker's
+// 'speak' method.
+// See Lecture 50, part 1
+for (/* fill in parts of the 'for' loop to loop over names array */) {
+
+  // STEP 11:
+  // Retrieve the first letter of the current name in the loop.
+  // Use the string object's 'charAt' function. Since we are looking for
+  // names that start with either upper case or lower case 'J'/'j', call
+  // string object's 'toLowerCase' method on the result so we can compare
+  // to lower case character 'j' afterwards.
+  // Look up these methods on Mozilla Developer Network web site if needed.
+  // var firstLetter =
+
+  // STEP 12:
+  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+  // name in the loop.
+  if (/* fill in condition here */) {
+    // byeSpeaker.xxxx
+  } else {
+    // helloSpeaker.xxxx
+  }
 }
-var doubleAll = makeMultiplier(2);
-console.log(doubleAll(15));
-// Closure allow the outer lexical environment to stay saved
-
-// Namespace (just an object, prevent variables overriding, esp. relevant when you have multiple scripts called in the HTML file)
-// script2 in his demo was the same except for John and different hello variable
-var yaakovGreeter = {};
-yaakovGreeter.name = "Yaakov";
-yaakovGreeter.sayHello = function() {
-	console.log("Hello " + yaakovGreeter.name);
-}
-yaakovGreeter.sayHello();
-
-//Immediately Invoked Function Expressions (IIFEs)
-(function () {
-	console.log("Hello Coursera")
-})(); //THIS LINE IS AN IIFE OR "IFFY"
-
-(function (window) {
-	var yaakovGeezy = {};
-	yaakovGeezy.name = "Yaakov";
-	window.yaakovGeezy = yaakovGeezy;
-})(window); //
-console.log(yaakovGeezy);
-//^THIS ALLOWS YOU TO IMMEDIATELY CALL, AND ALSO PUT IT IN GLOBAL WINDOW IN THIS OBJECT!
-
